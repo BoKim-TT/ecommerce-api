dd4dd258a09b4c34b68c4dfbe3de413e5a17b001
chloenamu <bkchloekim@gmail.com>
2023-01-18 16:41:19 -0500

    changed config for production mode

--- index.js
+++ index.js
@@ -35,10 +35,13 @@ if (process.env.NODE_ENV === "development") {
 }
 if (process.env.NODE_ENV === "production") {
   app.use(compression());
-  app.use(express.static(path.join(__dirname, "..", "/client/build")));
-  app.use(express.static(path.join(__dirname, "/public")));
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
+app.use(express.static(path.join(__dirname, "/public")));
 //---------------item endpoints ---------------------//
 //GET all items
 //returns an array if objects
